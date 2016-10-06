e098ef7d59d57e836d4b907bd6b3e807bf1406c6
pcjellison <pcjellison@ksu.edu>
2016-10-06 22:41:22 +0900

    Add files via upload

---
+++ Lab11.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_1 = new System.Windows.Forms.Button();
+            this.button_2 = new System.Windows.Forms.Button();
+            this.button_3 = new System.Windows.Forms.Button();
+            this.button_4 = new System.Windows.Forms.Button();
+            this.button_5 = new System.Windows.Forms.Button();
+            this.button_6 = new System.Windows.Forms.Button();
+            this.button_7 = new System.Windows.Forms.Button();
+            this.button_8 = new System.Windows.Forms.Button();
+            this.button_9 = new System.Windows.Forms.Button();
+            this.button_0 = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.answerText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_1
+            // 
+            this.button_1.AccessibleName = "";
+            this.button_1.Location = new System.Drawing.Point(12, 109);
+            this.button_1.Name = "button_1";
+            this.button_1.Size = new System.Drawing.Size(75, 23);
+            this.button_1.TabIndex = 0;
+            this.button_1.Text = "1";
+            this.button_1.UseVisualStyleBackColor = true;
+            // 
+            // button_2
+            // 
+            this.button_2.AccessibleName = "";
+            this.button_2.Location = new System.Drawing.Point(107, 109);
+            this.button_2.Name = "button_2";
+            this.button_2.Size = new System.Drawing.Size(75, 23);
+            this.button_2.TabIndex = 1;
+            this.button_2.Text = "2";
+            this.button_2.UseVisualStyleBackColor = true;
+            // 
+            // button_3
+            // 
+            this.button_3.AccessibleName = "";
+            this.button_3.Location = new System.Drawing.Point(224, 109);
+            this.button_3.Name = "button_3";
+            this.button_3.Size = new System.Drawing.Size(75, 23);
+            this.button_3.TabIndex = 2;
+            this.button_3.Text = "3";
+            this.button_3.UseVisualStyleBackColor = true;
+            // 
+            // button_4
+            // 
+            this.button_4.AccessibleName = "";
+            this.button_4.Location = new System.Drawing.Point(12, 188);
+            this.button_4.Name = "button_4";
+            this.button_4.Size = new System.Drawing.Size(75, 23);
+            this.button_4.TabIndex = 3;
+            this.button_4.Text = "4";
+            this.button_4.UseVisualStyleBackColor = true;
+            // 
+            // button_5
+            // 
+            this.button_5.AccessibleName = "";
+            this.button_5.Location = new System.Drawing.Point(107, 188);
+            this.button_5.Name = "button_5";
+            this.button_5.Size = new System.Drawing.Size(75, 23);
+            this.button_5.TabIndex = 4;
+            this.button_5.Text = "5";
+            this.button_5.UseVisualStyleBackColor = true;
+            // 
+            // button_6
+            // 
+            this.button_6.AccessibleName = "";
+            this.button_6.Location = new System.Drawing.Point(224, 188);
+            this.button_6.Name = "button_6";
+            this.button_6.Size = new System.Drawing.Size(75, 23);
+            this.button_6.TabIndex = 5;
+            this.button_6.Text = "6";
+            this.button_6.UseVisualStyleBackColor = true;
+            // 
+            // button_7
+            // 
+            this.button_7.AccessibleName = "";
+            this.button_7.Location = new System.Drawing.Point(12, 290);
+            this.button_7.Name = "button_7";
+            this.button_7.Size = new System.Drawing.Size(75, 23);
+            this.button_7.TabIndex = 6;
+            this.button_7.Text = "7";
+            this.button_7.UseVisualStyleBackColor = true;
+            // 
+            // button_8
+            // 
+            this.button_8.AccessibleName = "";
+            this.button_8.Location = new System.Drawing.Point(107, 290);
+            this.button_8.Name = "button_8";
+            this.button_8.Size = new System.Drawing.Size(75, 23);
+            this.button_8.TabIndex = 7;
+            this.button_8.Text = "8";
+            this.button_8.UseVisualStyleBackColor = true;
+            // 
+            // button_9
+            // 
+            this.button_9.AccessibleName = "";
+            this.button_9.Location = new System.Drawing.Point(224, 290);
+            this.button_9.Name = "button_9";
+            this.button_9.Size = new System.Drawing.Size(75, 23);
+            this.button_9.TabIndex = 8;
+            this.button_9.Text = "9";
+            this.button_9.UseVisualStyleBackColor = true;
+            // 
+            // button_0
+            // 
+            this.button_0.AccessibleName = "";
+            this.button_0.Location = new System.Drawing.Point(12, 378);
+            this.button_0.Name = "button_0";
+            this.button_0.Size = new System.Drawing.Size(75, 23);
+            this.button_0.TabIndex = 9;
+            this.button_0.Text = "0";
+            this.button_0.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(162, 378);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 10;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // answerText
+            // 
+            this.answerText.AccessibleName = "";
+            this.answerText.Enabled = false;
+            this.answerText.Location = new System.Drawing.Point(23, 45);
+            this.answerText.Name = "answerText";
+            this.answerText.ReadOnly = true;
+            this.answerText.Size = new System.Drawing.Size(270, 20);
+            this.answerText.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 446);
+            this.Controls.Add(this.answerText);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.button_0);
+            this.Controls.Add(this.button_9);
+            this.Controls.Add(this.button_8);
+            this.Controls.Add(this.button_7);
+            this.Controls.Add(this.button_6);
+            this.Controls.Add(this.button_5);
+            this.Controls.Add(this.button_4);
+            this.Controls.Add(this.button_3);
+            this.Controls.Add(this.button_2);
+            this.Controls.Add(this.button_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_1;
+        private System.Windows.Forms.Button button_2;
+        private System.Windows.Forms.Button button_3;
+        private System.Windows.Forms.Button button_4;
+        private System.Windows.Forms.Button button_5;
+        private System.Windows.Forms.Button button_6;
+        private System.Windows.Forms.Button button_7;
+        private System.Windows.Forms.Button button_8;
+        private System.Windows.Forms.Button button_9;
+        private System.Windows.Forms.Button button_0;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.TextBox answerText;
+    }
+}
+
---
+++ Lab11.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Payton Jellison
+ * Varun Varshnay 7:30am Friday
+ * This lab creates a calculator allowing the user to click numbers and clear numbers.
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        private Button[] buttons;
+        public Form1()
+        {
+            InitializeComponent();
+            buttons = new Button[10];
+            buttons[0] = button_0;
+            buttons[0].Click += new EventHandler(numberClick);
+            buttons[1] = button_1;
+            buttons[1].Click += new EventHandler(numberClick);
+            buttons[2] = button_2;
+            buttons[2].Click += new EventHandler(numberClick);
+            buttons[3] = button_3;
+            buttons[3].Click += new EventHandler(numberClick);
+            buttons[4] = button_4;
+            buttons[4].Click += new EventHandler(numberClick);
+            buttons[5] = button_5;
+            buttons[5].Click += new EventHandler(numberClick);
+            buttons[6] = button_6;
+            buttons[6].Click += new EventHandler(numberClick);
+            buttons[7] = button_7;
+            buttons[7].Click += new EventHandler(numberClick);
+            buttons[8] = button_8;
+            buttons[8].Click += new EventHandler(numberClick);
+            buttons[9] = button_9;
+            buttons[9].Click += new EventHandler(numberClick);
+            clearButton.Click += new EventHandler(clearClick);
+            
+
+        }
+        private void numberClick(object sender, EventArgs e)
+        {
+            if (answerText.Text.Length < 14)        //Allows for 15 numbers
+            {
+                Button b = (Button)sender;
+                answerText.Text += b.Text;
+            }
+            else
+            {
+                for (int i = 0; i < buttons.Length; i++)
+                {
+                    buttons[i].Enabled = false;
+                }
+            }
+        }
+
+        private void clearClick(object sender, EventArgs e)
+        {
+            answerText.Text = "";
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].Enabled = true;
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
